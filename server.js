8a4ecd628c2383c7a261ed685803dc7ac8705126
Oduor Samuel <soduor15@yahoo.com>
2019-07-03 17:29:01 +0300

    added update endpoint

--- server.js
+++ server.js
@@ -50,5 +50,25 @@ router.route('/courses/add').post((req, res) => {
         });
 });
 
+router.route('/courses/update/:id').post((req, res) => {
+    Courses.findById(req.params.id, (err, Courses) => {
+        if (!Courses)
+            return (err)
+        else {
+            Courses.courseName = req.body.courseName;
+            Courses.shortName = req.body.shortName;
+            Courses.description = req.body.description;
+            Courses.note = req.body.note;
+            Courses.questionIntro = req.body.questionIntro;
+            Courses.validPeriod = req.body.validPeriod;
+            Courses.save().then(Courses => {
+                res.json('course updated done');
+            }).catch(err => {
+                res.status(400).send('update failed');
+            });
+        }
+    });
+});
+
 app.use('/', router);
 app.listen(4000, () => console.log('Server running on port 4000'));

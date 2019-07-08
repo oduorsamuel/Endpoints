8a49e15077bfb8319eb7e0ac476360e664119241
Oduor Samuel <soduor15@yahoo.com>
2019-07-08 11:19:01 +0300

    created model for lessons

---
+++ models/Lesson.js
@@ -0,0 +1,39 @@
+const mongoose=require("mongoose");
+const Schema= mongoose.Schema;
+
+let lesson=new Schema({
+ lessonName:{
+     type:String,
+     required:true
+ },
+ startDate:{
+     type:Date,
+     default:Date.now()   
+ },
+ signUpStartDate:{
+    type:Date,
+    default:Date.now()   
+},
+signUpEndDate:{
+    type:Date,
+    default:Date.now()   
+},
+endDate:{
+    type:Date,
+    default:Date.now()  
+},
+ passWithin:{
+     type:Number,  
+     required:true
+ },
+ course:{
+     type:mongoose.Schema.Types.ObjectId, ref:'Courses',
+     required:true
+ },
+ teacher:{
+     type:String,
+     required:true
+ }
+},
+{ versionKey: false })
+module.exports=Courses=mongoose.model('Lessons',lesson);
